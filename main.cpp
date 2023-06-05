213ee35142236ce016fcc90384c97d48415c1629
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-05 08:07:53 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+#include "HEPTA_CDH.h"
+HEPTA_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd");
+Serial pc(USBTX,USBRX,9600);
+int main()
+{
+    pc.printf("Hello World!\n");
+ 
+    mkdir("/sd/mydir", 0777);
+ 
+    FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
+    if(fp == NULL) {
+        error("Could not open file for write\r\n");
+    }
+    fprintf(fp, "Hello my name is HEPTA\r\n");
+    fclose(fp);
+ 
+    pc.printf("Goodbye World!\r\n");
 }
