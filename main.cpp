69fb4b8a5c4e24128ef444a207358bfd3558a99a
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-08 02:59:41 +0000

    commit68

--- main.cpp
+++ main.cpp
@@ -5,15 +5,19 @@ Serial pc(USBTX,USBRX,9600);
 int main()
 {
     pc.printf("Hello World!\n");
- 
+    char str[100];
     mkdir("/sd/mydir", 0777);
  
     FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
     if(fp == NULL) {
         error("Could not open file for write\r\n");
     }
-    fprintf(fp, "Hello my name is HEPTA\r\n");
+    for (int i=0; i<10; i++)fprintf(fp,"Hello my name is HEPTA\r\n");
     fclose(fp);
- 
+    fp = fopen("/sd/mydir/sdtest.txt", "r");
+    for (int j =0; j < 10; j++){
+        fgets(str,100,fp);
+        pc.puts(str);
+    }
     pc.printf("Goodbye World!\r\n");
 }
