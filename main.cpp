5199f9ab76bda22b5d0eb8a0690b4bbc03f87343
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 04:57:04 +0000

    add fclose

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
 #include "mbed.h"
 #include "LITE_CDH.h"
+
 LITE_CDH cdh(PB_5, PB_4, PB_3, PA_8, "sd", PA_3);
 RawSerial pc(USBTX,USBRX,9600);
+
 int main()
 {
-    pc.printf("Hello World!\n");
+    pc.printf("Hello world!\r\n");
     char str[100];
     mkdir("/sd/mydir", 0777);
- 
-    FILE *fp = fopen("/sd/mydir/sdtest.txt", "w");
+    FILE *fp = fopen("/sd/mydir/test.txt","w");
     if(fp == NULL) {
         error("Could not open file for write\r\n");
     }
-    for (int i=0; i<10; i++)fprintf(fp,"Hello my name is HEPTA\r\n");
+    for(int i=0; i<10; i++)fprintf(fp,"Hello my name is HEPTA!\r\n");
     fclose(fp);
-    fp = fopen("/sd/mydir/sdtest.txt", "r");
-    for (int j =0; j < 10; j++){
+    fp = fopen("/sd/mydir/test.txt","r");
+    for(int j = 0; j < 10; j++) {
         fgets(str,100,fp);
         pc.puts(str);
     }
-    pc.printf("Goodbye World!\r\n");
+    fclose(fp);
+    pc.printf("Goodbye!!\r\n");
 }
